8e1aaa538709086379e27772bace05de627ea0de
William Gomes <williamjog@hotmail.com>
2021-10-07 07:33:12 -0300

    Versao 3.0

--- src/components/ListaDeMusicasLocalStorage.jsx
+++ src/components/ListaDeMusicasLocalStorage.jsx
@@ -7,7 +7,6 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-
 const ListaDeMusicasLocalStorage = () => {
   const { listasSalvas, setListasSalvas, setPesquisando } = useContext(Context);
 
