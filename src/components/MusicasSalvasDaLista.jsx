c9dce506c38524086c6d9312b4b4d87468bb2611
William Gomes <williamjog@hotmail.com>
2021-10-07 06:40:29 -0300

    Versao 2.0

---
+++ src/components/MusicasSalvasDaLista.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const MusicasSalvasDaLista = ({ lista }) => {
+  
+  return (
+    <div>
+      <div className="tituloCard">
+        Músicas:
+      </div>
+      { lista.musicas && lista.musicas.map((musica, index) => (
+        <div key={index}> 
+          <div> 
+            <span className="infoMusica">
+              {`${musica.titulo} | Artista: ${musica.artista}`}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default MusicasSalvasDaLista
